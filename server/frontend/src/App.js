a56ee9fdddfb3bd6961ae616b43177fb171d5275
Seemsbetter <149608496+Seemsbetter@users.noreply.github.com>
2025-01-09 15:50:22 +0100

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
+       <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
